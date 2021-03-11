22c5f430a1c665eeef5965ac3f6c012048a09513
Joey van Eijk <24193975+JoeyVE1996@users.noreply.github.com>
2021-03-11 10:18:25 +0100

    Add files via upload

---
+++ Objects/NPC.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    public class NPC
+    {
+        public NPC()
+        {
+
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string[] dialouge;
+        public string[] Dialouge
+        {
+            get { return dialouge; }
+            set { dialouge = value; }
+        }
+    }
+}
---
+++ Objects/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    public class Player {
+        public Player(string name)
+        {
+            this.Name = name;
+        }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private Item[] inventory;
+        public Item[] Inventory
+        {
+            get { return inventory; }
+            set { inventory = value; }
+        }
+    }
+}
